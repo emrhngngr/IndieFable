b1db0e1e6db27fa39f07058735ed5cb740ec550f
emrhngngr <emirhan_gungor2009@hotmail.com>
2025-10-13 23:31:21 +0300

    Add components and pages

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,19 @@
+﻿import { Button } from "@/components/ui/button";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="w-full h-16 text-white flex items-center justify-between px-10">
+        <a href="/" className="text-2xl cursor-pointer" >IndieFable</a>
+        <div>
+          <ul className="flex space-x-20 text-lg">
+            <Button className=" bg-white/10 px-6 py-2 cursor-pointer backdrop-blur-sm hover:bg-white/25 rounded text-glow-hover">Join to Waitlist</Button>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
